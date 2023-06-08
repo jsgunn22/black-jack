6fa7e8dac8c39ba68c2e74fbaad9c949fcccd0fa
jgunn <j.s.gunn22@gmail.com>
2023-06-08 13:43:08 -0700

    quick fix

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ function newGame() {
 
             yourTotal(yourHand)
 
-            var yourTotal = function yourTotal(yourHand) {
+            function yourTotal(yourHand) {
                 
                 yourHandNumbers = []
         
