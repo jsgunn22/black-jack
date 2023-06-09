a422d4dfa69baee476553ee1fd47b25068cb9181
jgunn <j.s.gunn22@gmail.com>
2023-06-09 14:17:27 -0700

    got the total to add on hit

--- index.js
+++ index.js
@@ -1,103 +1,114 @@
-document.querySelector("#newGame").addEventListener("click", newGame)
+document.querySelector("#newGame").addEventListener("click", newGame);
 
-
-
-var suit = ["❤️", "♦️", "♠️", "♣️"]
-var cardNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-
+var suit = ["❤️", "♦️", "♠️", "♣️"];
+var cardNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
 function newGame() {
-    
-    
-    var dealersHand = dealersCards();
-    var yourHand = yourCards();
+  var dealersHand = dealersCards();
+  var yourHand = yourCards();
 
-    yourTotal(yourHand)
-    dealerTotal(dealersHand)
+  yourTotal(yourHand);
+  dealerTotal(dealersHand);
 
-            function yourTotal(yourHand) {
-                        
-                yourHandNumbers = []
+  function dealersCards() {
+    var dealersHand = [];
+    for (let i = 0; i < 2; i++) {
+      var getSuit = suit[Math.floor(Math.random() * 4)];
+      var getCardNumber = cardNumber[Math.floor(Math.random() * 13)];
+      if (getCardNumber > 10) {
+        getCardNumber = 10;
+      } else {
+        getCardNumber = getCardNumber;
+      }
+      dealersHand.push(getCardNumber + getSuit);
+    }
+    return dealersHand;
+  }
 
-                for (let i = 0; i < yourHand.length; i++) {
-                    yourHandNumbers.push(Number(yourHand[i].slice(0, yourHand[i].length - 2)))
-                }
+  function yourCards() {
+    var yourHand = [];
+    for (let i = 0; i < 2; i++) {
+      var getSuit = suit[Math.floor(Math.random() * 4)];
+      var getCardNumber = cardNumber[Math.floor(Math.random() * 13)];
+      if (getCardNumber > 10) {
+        getCardNumber = 10;
+      } else {
+        getCardNumber = getCardNumber;
+      }
+      yourHand.push(getCardNumber + getSuit);
+    }
+    return yourHand;
+  }
 
-                yourTotal = yourHandNumbers.reduce((a, b) => {
-                    return a + b;
-                })
+  document.querySelector("#hit-button").addEventListener("click", hitMe);
 
-                document.getElementById("yourTotal").innerHTML = yourTotal
-            }
+  document.getElementById("dealerHand").innerHTML = dealersHand;
+  document.getElementById("yourHand").innerHTML = yourHand;
 
+  function dealerTotal(dealersHand) {
+    dealerHandNumbers = [];
 
+    for (let i = 0; i < dealersHand.length; i++) {
+      dealerHandNumbers.push(
+        Number(dealersHand[i].slice(0, dealersHand[i].length - 2))
+      );
+    }
 
-            function dealersCards() {
-                var dealersHand = []
-                for (let i = 0; i < 2; i++) {
-                    var getSuit = suit[Math.floor(Math.random() * 4)]
-                    var getCardNumber = cardNumber[Math.floor(Math.random() * 13)]
-                    if (getCardNumber > 10) {
-                        getCardNumber = 10
-                    } else {
-                    getCardNumber = getCardNumber
-                    }
-                    dealersHand.push(getCardNumber + getSuit)
-                    }
-                return (dealersHand)
-            }
+    dealerTotal = dealerHandNumbers.reduce((a, b) => {
+      return a + b;
+    });
 
-            function yourCards() {
-                var yourHand = []
-                for (let i = 0; i < 2; i++) {
-                    var getSuit = suit[Math.floor(Math.random() * 4)]
-                    var getCardNumber = cardNumber[Math.floor(Math.random() * 13)]
-                    if (getCardNumber > 10) {
-                        getCardNumber = 10
-                    } else {
-                    getCardNumber = getCardNumber
-                    }
-                    yourHand.push(getCardNumber + getSuit)
-                    }
-                return (yourHand)
-            }
-          
-            document.querySelector("#hit-button").addEventListener("click", hitMe) 
+    document.getElementById("dealerTotal").innerHTML = dealerTotal;
+  }
 
-            function hitMe() {
-                    var getSuit = suit[Math.floor(Math.random() * 4)]
-                    var getCardNumber = cardNumber[Math.floor(Math.random() * 13)]
-                    if (getCardNumber > 10) {
-                        getCardNumber = 10
-                    } else {
-                    getCardNumber = getCardNumber
-                    }
+  function yourTotal(yourHand) {
+    yourHandNumbers = [];
 
-                    yourHand.push(getCardNumber + getSuit)
-                    document.getElementById("yourHand").innerHTML = yourHand
-                   
+    for (let i = 0; i < yourHand.length; i++) {
+      yourHandNumbers.push(
+        Number(yourHand[i].slice(0, yourHand[i].length - 2))
+      );
+    }
 
-            } 
+    yourTotal = yourHandNumbers.reduce((a, b) => {
+      return a + b;
+    });
+    if (yourTotal === 21) {
+      document.getElementById("yourTotal").innerHTML =
+        yourTotal + " Black Jack!!";
+    } else {
+      document.getElementById("yourTotal").innerHTML = yourTotal;
+    }
+  }
 
-            document.getElementById("dealerHand").innerHTML = dealersHand
-            document.getElementById("yourHand").innerHTML = yourHand
+  function hitMe() {
+    var getSuit = suit[Math.floor(Math.random() * 4)];
+    var getCardNumber = cardNumber[Math.floor(Math.random() * 13)];
+    if (getCardNumber > 10) {
+      getCardNumber = 10;
+    } else {
+      getCardNumber = getCardNumber;
+    }
 
-            function dealerTotal(dealersHand) {
-                
-                dealerHandNumbers = []
+    yourHand.push(getCardNumber + getSuit);
+    document.getElementById("yourHand").innerHTML = yourHand;
+    yourHandNumbers = [];
 
-                for (let i = 0; i < dealersHand.length; i++) {
-                    dealerHandNumbers.push(Number(dealersHand[i].slice(0, dealersHand[i].length - 2)))
-                }
+    for (let i = 0; i < yourHand.length; i++) {
+      yourHandNumbers.push(
+        Number(yourHand[i].slice(0, yourHand[i].length - 2))
+      );
+    }
+    // This section inside of the hit button rebuild the players total based on the new array
+    yourTotal = yourHandNumbers.reduce((a, b) => {
+      return a + b;
+    });
 
-                dealerTotal = dealerHandNumbers.reduce((a, b) => {
-                    return a + b;
-                })
-
-                document.getElementById("dealerTotal").innerHTML = dealerTotal
-            }
-
-            
-
-
+    if (yourTotal > 21) {
+      document.getElementById("yourTotal").innerHTML =
+        yourTotal + " You busted, Bro...";
+    } else {
+      document.getElementById("yourTotal").innerHTML = yourTotal;
+    }
+  }
 }
