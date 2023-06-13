45482de2fd09ec4e5fa29ff93b5288bcc21800a4
jgunn <j.s.gunn22@gmail.com>
2023-06-13 13:45:23 -0700

    creates seven decks and shuffles all of them together into shuffledDecks

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const suits = ["❤️", "♦️", "♠️", "♣️"];
 const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
 const face = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
-var sevenDecks = [];
+document.querySelector("#shuffleDeck").addEventListener("click", shuffleDecks);
+
 var shuffledDeck = [];
 
-function shuffleDeck() {
+function shuffleDecks() {
+  shuffledDeck = []; // this clears the array on a new shuffle
+  var sevenDecks = [];
+
+  // creates seven decks to fill the sevenDecks array
   for (let d = 0; d < 7; d++) {
     for (let s = 0; s < suits.length; s++) {
       for (let v = 0; v < values.length; v++) {
@@ -18,13 +23,24 @@ function shuffleDeck() {
       }
     }
   }
-  for (let i = 0; i < sevenDecks.length; i++) {
-    var randomCard = sevenDecks[Math.floor(Math.random() * sevenDecks.length)];
 
-    if (randomCard != shuffledDeck.includes()) {
-      shuffledDeck.push(randomCard);
-    } else {
-      i = i - 1;
-    }
+  // shuffles sevenDecks and pushes each card to shuffledDecks
+  var temp = [];
+  while (shuffledDeck.length < 364) {
+    temp.push(
+      sevenDecks.splice(Math.floor(Math.random() * sevenDecks.length - 1), 1)[0]
+    );
+    shuffledDeck.push(temp.pop());
   }
+
+  // this section is to test print the shuffled deck
+  // for (let i = 0; i < shuffledDeck.length; i++) {
+  //   var tag = document.createElement("p");
+  //   var text = document.createTextNode(
+  //     shuffledDeck[i].face + shuffledDeck[i].suit
+  //   );
+  //   tag.appendChild(text);
+  //   var element = document.getElementById("new");
+  //   element.appendChild(tag);
+  // }
 }
